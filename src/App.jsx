e477b30a66f6ee6ba3fe9353213b3ccacbe1ecee
Serhat Kılbaş <serhatkilbas127@gmail.com>
2024-12-14 00:16:42 +0300

    Implement initial design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './index.css'
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [word, setWord] = useState("react")
+
+  const wordElement = word.split("").map(letter => {
+    return <span key={uuidv4()} className='letter'>{letter.toUpperCase()}</span>
+  })
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  const keyboardElement = alphabet.split("").map(letter => {
+    return <button key={uuidv4()} className='key'>{letter.toUpperCase()}</button>
+  })
+
   return (
     <main>
       <header className='header'>
-        <h1>Assembly: Endgame</h1>
-        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+        <h1>WORD MYSTERY</h1>
+        <p>Guess the word in under 8 attempts</p>
       </header>
 
       <section className='status'>
@@ -18,11 +31,11 @@ function App() {
       </section>
 
       <section className='word'>
-        {letterElements}
+        {wordElement}
       </section>
 
       <section className="keyboard">
-        {keyboardElements}
+        {keyboardElement}
       </section>
     </main>
   )

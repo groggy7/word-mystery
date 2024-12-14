f07f110de979c38536031ab1352f0a955e4e57dd
Serhat Kılbaş <serhatkilbas127@gmail.com>
2024-12-14 13:48:18 +0300

    Conditionally display the word
    Implement keyboard functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,38 @@ import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [word, setWord] = useState("react")
+  const [guessedLetters, setGuessedLetters] = useState([])
 
   const wordElement = word.split("").map(letter => {
-    return <span key={uuidv4()} className='letter'>{letter.toUpperCase()}</span>
+    return <span 
+      key={uuidv4()}
+      className='letter'
+      >{guessedLetters.includes(letter) ? letter.toUpperCase() : ''}
+    </span>
   })
 
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
   const keyboardElement = alphabet.split("").map(letter => {
-    return <button key={uuidv4()} className='key'>{letter.toUpperCase()}</button>
+    const isGuessed = guessedLetters.includes(letter)
+    const isCorrect = word.split("").includes(letter)
+    
+    const styles = {
+      backgroundColor: isGuessed ? isCorrect ? "#10A95B" : "#EC5D49" : ""
+    }
+
+    return <button 
+      key={uuidv4()}
+      className='key'
+      style={styles}
+      onClick={() => addGuessedLetter(letter)}
+    >{letter.toUpperCase()}
+    </button>
   })
 
+  function addGuessedLetter(letter) {
+    setGuessedLetters(prev => [...prev, letter])
+  }
+  
   return (
     <main>
       <header className='header'>

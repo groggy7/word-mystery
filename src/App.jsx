255cca26a774805405a50c0b934ae9d64d52d5d3
Serhat Kılbaş <serhatkilbas127@gmail.com>
2024-12-14 21:19:45 +0300

    Add game over status

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [word, setWord] = useState("react")
   const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongGuessCount, setWrongGuessCount] = useState(0)
+
+  const isGameWon = word.split("").every(letter => guessedLetters.includes(letter))
+  const isGameLost = wrongGuessCount >= 8
+  const isGameOver = isGameWon || isGameLost
 
   const wordElement = word.split("").map(letter => {
     return <span 
@@ -18,9 +23,11 @@ function App() {
   const keyboardElement = alphabet.split("").map(letter => {
     const isGuessed = guessedLetters.includes(letter)
     const isCorrect = word.split("").includes(letter)
-    
+    const isDisabled = isGameOver || isGuessed
+
     const styles = {
-      backgroundColor: isGuessed ? isCorrect ? "#10A95B" : "#EC5D49" : ""
+      backgroundColor: isGuessed ? isCorrect ? "#10A95B" : "#EC5D49" : "",
+      cursor: isDisabled ? "not-allowed" : "pointer"
     }
 
     return <button 
@@ -28,11 +35,13 @@ function App() {
       className='key'
       style={styles}
       onClick={() => addGuessedLetter(letter)}
+      disabled={isDisabled}
     >{letter.toUpperCase()}
     </button>
   })
 
   function addGuessedLetter(letter) {
+    !word.split("").includes(letter) ? setWrongGuessCount(prev => prev+1) : null
     setGuessedLetters(prev => [...prev, letter])
   }
   
@@ -43,13 +52,27 @@ function App() {
         <p>Guess the word in under 8 attempts</p>
       </header>
 
-      <section className='status'>
-        <h3>You win!</h3>
-        <h4>Well done! 🎉</h4>
+      <section 
+        className='status'
+        style={{
+          display: isGameOver ? "block" : "none"
+        }}
+      >
+        {isGameWon ? (
+          <>
+            <h3>You win!</h3>
+            <h4>Well done! 🎉</h4>
+          </>
+        ) : isGameLost ? (
+          <>
+            <h3>Game Over!</h3>
+            <h4>Better luck next time! 😊</h4>
+          </>
+        ) : null}
       </section>
 
       <section className='count'>
-        <p>You have 8 wrong guess left!</p>
+        <p>You have {8 - wrongGuessCount} wrong guess left!</p>
       </section>
 
       <section className='word'>
